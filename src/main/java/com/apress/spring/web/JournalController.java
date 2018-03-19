ceaf92d426b98768839a89bdcb5bcc189823676a
Согомонян Вахтанг <v.sogomonyan@okenit.ru>
2018-03-19 15:43:36 +0300

    Spring Boot Web configuring

--- src/main/java/com/apress/spring/web/JournalController.java
+++ src/main/java/com/apress/spring/web/JournalController.java
@@ -1,19 +1,63 @@
 package com.apress.spring.web;
 
-import com.apress.spring.repository.JournalRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.apress.spring.domain.JournalEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-public class JournalController {
-    @Autowired
-    JournalRepository repo;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("journal", repo.findAll());
-        return "index";
+@RestController
+public class    JournalController {
+    private static List<JournalEntry> entries = new ArrayList<>();
+
+//    static {
+//        try {
+//            entries.add(new JournalEntry(
+//                    "Get to know Spring Boot",
+//                    "Today I will learn Spring Boot",
+//                    "01/01/2016"));
+//            entries.add(new JournalEntry(
+//                    "Simple Spring Boot Project",
+//                    "I will do my first Spring Boot Project",
+//                    "01/02/2016"));
+//            entries.add(new JournalEntry(
+//                    "Spring Boot Reading",
+//                    "Read more about Spring Boot",
+//                    "02/01/2016"));
+//            entries.add(new JournalEntry(
+//                    "Spring Boot in the Cloud",
+//                    "Spring Boot using Cloud Foundry",
+//                    "03/01/2016"));
+//            entries.add(new JournalEntry(
+//                    "Test Spring Boot",
+//                    "Create Unit Test for Spring Boot",
+//                    "06/18/2016"));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    @RequestMapping("/journal/all")
+    public List<JournalEntry> getAll() throws ParseException {
+        return entries;
+    }
+
+    @RequestMapping("/journal/findBy/title/{title}")
+    public List<JournalEntry> findByTitleContains(@PathVariable String title) throws ParseException {
+        return entries
+                .stream()
+                .filter(entry -> entry.getTitle().toLowerCase().contains(title.
+                        toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/journal", method = RequestMethod.POST)
+    public JournalEntry add(@RequestBody JournalEntry entry) {
+        entries.add(entry);
+        return entry;
     }
 }
